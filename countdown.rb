72f48c784d115b3bbc4c4f67251aba10dfb7e56d
Kyle Talmage <ktalmage85@gmail.com>
2020-02-11 14:32:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,13 +1,13 @@
 #write your code here
 
-#def countdown
+def countdown
   number = 10
-  #while number > 0
-   # puts "#{number} SECOND(S)!"
+  while number > 0
+   puts "#{number} SECOND(S)!"
     number -= 1
-    #end
-  #if number = 0
-   # puts "HAPPY NEW YEAR!"
+    end
+  if number = 0
+  puts "HAPPY NEW YEAR!"
   #end
 #end
 
